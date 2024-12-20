3357a22317d00e2574741c6b7334569fdb54585e
Lilien <lilienauger.86@gmail.com>
2024-12-20 16:58:15 +0100

    initial push

---
+++ foodWars.py
@@ -0,0 +1,371 @@
+import os
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision import datasets, transforms
+
+from typing import Tuple, Dict, List
+from tqdm.auto import tqdm
+from timeit import default_timer as timer
+
+# from utils import plot_transformed_images
+
+from pathlib import Path
+import pathlib
+import random
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+from printFunctions import plot_loss_curves
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+data_path = Path("data/")
+image_path = data_path / "pizza_steak_sushi"
+
+train_dir = image_path / "train"
+test_dir = image_path / "test"
+
+target_directory = train_dir
+
+def find_classes(directory: str) -> Tuple[List[str], Dict[str, int]]:
+    """Finds the class folder names in a target directory.
+    
+    Assumes target directory is in standard image classification format.
+
+    Args:
+        directory (str): target directory to load classnames from.
+
+    Returns:
+        Tuple[List[str], Dict[str, int]]: (list_of_class_names, dict(class_name: idx...))
+    
+    Example:
+        find_classes("food_images/train")
+        >>> (["class_1", "class_2"], {"class_1": 0, ...})
+    """
+    classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+    
+    if not classes:
+        raise FileNotFoundError(f"Couldn't find any classes in {directory}.")
+        
+    class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+    return classes, class_to_idx
+
+class ImageFolderCustom(Dataset):
+    
+    def __init__(self, targ_dir: str, transform=None) -> None:
+        
+        self.paths = list(pathlib.Path(targ_dir).glob("*/*.jpg"))
+        self.transform = transform
+        self.classes, self.class_to_idx = find_classes(targ_dir)
+
+    def load_image(self, index: int) -> Image.Image:
+        "Opens an image via a path and returns it."
+        image_path = self.paths[index]
+        return Image.open(image_path) 
+    
+    def __len__(self) -> int:
+        "Returns the total number of samples."
+        return len(self.paths)
+    
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+        "Returns one sample of data, data and label (X, y)."
+        img = self.load_image(index)
+        class_name  = self.paths[index].parent.name
+        class_idx = self.class_to_idx[class_name]
+
+        if self.transform:
+            return self.transform(img), class_idx
+        else:
+            return img, class_idx
+        
+
+#######################
+###### Data Transforms
+#######################
+train_transforms = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.TrivialAugmentWide(num_magnitude_bins=31),
+    transforms.ToTensor()
+])
+
+train_transform_trivial_augment = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.TrivialAugmentWide(num_magnitude_bins=31),
+    transforms.ToTensor() 
+])
+
+test_transforms = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
+
+simple_transform = transforms.Compose([ 
+    transforms.Resize((64, 64)),
+    transforms.ToTensor(),
+])
+
+#######################
+###### Load Data
+#######################
+
+train_data = ImageFolderCustom(targ_dir=train_dir, 
+                                      transform=train_transforms)
+test_data = ImageFolderCustom(targ_dir=test_dir, 
+                                     transform=test_transforms)
+
+def display_random_images(dataset: torch.utils.data.dataset.Dataset,
+                          classes: List[str] = None,
+                          n: int = 10,
+                          display_shape: bool = True,
+                          seed: int = None):
+    
+    if n > 10:
+        n = 10
+        display_shape = False
+        print(f"For display purposes, n shouldn't be larger than 10, setting to 10 and removing shape display.")
+    
+    if seed:
+        random.seed(seed)
+
+    random_samples_idx = random.sample(range(len(dataset)), k=n)
+
+    plt.figure(figsize=(16, 8))
+
+    for i, targ_sample in enumerate(random_samples_idx):
+        targ_image, targ_label = dataset[targ_sample][0], dataset[targ_sample][1]
+
+        targ_image_adjust = targ_image.permute(1, 2, 0)
+
+        plt.subplot(1, n, i+1)
+        plt.imshow(targ_image_adjust)
+        plt.axis("off")
+        if classes:
+            title = f"class: {classes[targ_label]}"
+            if display_shape:
+                title = title + f"\nshape: {targ_image_adjust.shape}"
+        plt.title(title)
+
+# display_random_images(train_data, 
+#                       n=10, 
+#                       classes=train_data.classes,
+#                       seed=42)
+# plt.show()
+
+train_dataloader_custom = DataLoader(dataset=train_data,
+                                     batch_size = 1,
+                                     num_workers = 0,
+                                     shuffle = True)
+
+
+test_dataloader_cusotm = DataLoader(dataset = test_data,
+                                    batch_size = 1,
+                                    num_workers = 0,
+                                    shuffle = True)
+#######################
+######TinyVGG Model
+#######################
+class TinyVGG(nn.Module):
+    """
+    Model architecture copying TinyVGG from: 
+    https://poloclub.github.io/cnn-explainer/
+    """
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*16*16,
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        return self.classifier(self.conv_block_2(self.conv_block_1(x)))
+
+
+#######################
+###### Train Model
+#######################
+def train_step(model: torch.nn.Module, 
+               dataloader: torch.utils.data.DataLoader, 
+               loss_fn: torch.nn.Module, 
+               optimizer: torch.optim.Optimizer):
+    model.train()
+    
+    train_loss, train_acc = 0, 0
+    
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item() 
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+def test_step(model: torch.nn.Module, 
+              dataloader: torch.utils.data.DataLoader, 
+              loss_fn: torch.nn.Module):
+    model.eval() 
+    
+    test_loss, test_acc = 0, 0
+    
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
+    
+            test_pred_logits = model(X)
+
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+            
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+             
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5):
+    
+    results = {"train_loss": [],
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": []
+    }
+
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer)
+        test_loss, test_acc = test_step(model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn)
+        
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        results["train_loss"].append(train_loss.item() if isinstance(train_loss, torch.Tensor) else train_loss)
+        results["train_acc"].append(train_acc.item() if isinstance(train_acc, torch.Tensor) else train_acc)
+        results["test_loss"].append(test_loss.item() if isinstance(test_loss, torch.Tensor) else test_loss)
+        results["test_acc"].append(test_acc.item() if isinstance(test_acc, torch.Tensor) else test_acc)
+
+    return results
+
+
+#######################
+######Test DataSet
+#######################
+if __name__ == "__main__":
+    print("Starting the script...")
+
+    try:
+        # Loading datasets
+        print("Loading datasets...")
+        train_data_simple = datasets.ImageFolder(root=train_dir, transform=simple_transform)
+        train_data_augmented = datasets.ImageFolder(train_dir, transform=train_transform_trivial_augment)
+        print(f"Train dataset loaded with {len(train_data_simple)} samples.")
+        test_data_simple = datasets.ImageFolder(root=test_dir, transform=simple_transform)
+        print(f"Test dataset loaded with {len(test_data_simple)} samples.")
+
+        # Creating DataLoaders
+        print("Creating DataLoaders...")
+        BATCH_SIZE = 32
+        NUM_WORKERS = 0
+        print(f"Using {NUM_WORKERS} workers.")
+        train_dataloader_augmented = DataLoader(train_data_augmented, 
+                                        batch_size=BATCH_SIZE, 
+                                        shuffle=True,
+                                        num_workers=NUM_WORKERS)
+
+        test_dataloader_simple = DataLoader(test_data_simple, 
+                                            batch_size=BATCH_SIZE, 
+                                            shuffle=False, 
+                                            num_workers=NUM_WORKERS)
+
+        # Initialize the model
+        print("Initializing the model...")
+        model_0 = TinyVGG(input_shape=3,
+                          hidden_units=10, 
+                          output_shape=len(train_data_simple.classes)).to(device)
+
+        # Training setup
+        loss_fn = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(params=model_0.parameters(), lr=0.001)
+        start_time = timer()
+
+        # Train the model
+        print("Starting training...")
+        model_0_results = train(model=model_0, 
+                                train_dataloader=train_dataloader_augmented,
+                                test_dataloader=test_dataloader_simple,
+                                optimizer=optimizer,
+                                loss_fn=loss_fn, 
+                                epochs=5)
+
+        # End training
+        end_time = timer()
+        print(f"Total training time: {end_time-start_time:.3f} seconds")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# print(model_0_results.keys())
+plot_loss_curves(model_0_results)
+
+MODEL_PATH = Path("model")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+MODEL_NAME = "food_wars.pth"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+print(f"Saving model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model_0_results.state_dict(), f=MODEL_SAVE_PATH)
